9fcdec26ef00529d1cfc59e740c8c138dd0b35a0
albertz03 <albertdezh@gmail.com>
2022-04-11 18:52:26 -0500

    se añaden ejemplos y ejercicios

---
+++ weekly_mission_1/example3/main.js
@@ -0,0 +1,9 @@
+/*
+  node main.js
+*/
+
+const logger1 = require('./logger_1')
+const logger2 = require('./logger_2')
+
+logger1.info('This is an informational message')
+logger2.verbose('This is a verbose message')
